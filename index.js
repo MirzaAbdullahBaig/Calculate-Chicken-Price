efa497b2299a59ee619156292646e44c94d4548d
Mirza Abdullah Baig <abaig7921gmail.com>
2022-10-30 07:55:21 +0500

    First commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let num1 = prompt("How many KG of Chicken you have purchased?")
+let num2 = prompt("How much is one kilogram of chicken?")
+
+let num3 = alert(`You have given money ${num1 * num2}`)
+
+
+let num4 = prompt("At what price are you giving a kilogram of chicken?")
+let num5 = prompt("How much you have to take?")
+
+let num6 = alert(`Plz pay ${num4 * num5}`)
